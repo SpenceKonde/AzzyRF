d16719f051ad55294b02cc37c4d594c1c4859d85
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2019-11-13 17:28:58 -0500

    Create AzzyRFRXPins.h

---
+++ OutletControl/AzzyRFRXPins.h
@@ -0,0 +1,15 @@
+//Microcontroller-specific
+
+#ifdef __AVR_ATmega328P__
+#define RX_PIN_STATE (PINB&1) //RX on pin 8 for input capture. 
+#endif
+
+#if defined(__AVR_ATtinyx16__) || defined(__AVR_ATtinyx06__)
+#define RX_PIN_STATE (VPORTA.IN&2) //RX on pin A1 for input capture.  pin 14
+#define RX_ASYNC0 0x0B
+#endif
+
+#if defined(__AVR_ATtinyx12__) || defined(__AVR_ATtinyx02__)
+#define RX_PIN_STATE (VPORTA.IN&8) //RX on pin A3 for input capture.  pin 4
+#define RX_ASYNC0 0x0D
+#endif
